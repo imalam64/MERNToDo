0b2bfbc23e0cf9930d565170202ca624b6d51299
imalam64 <imalam64@gmail.com>
2018-11-16 21:03:31 -0800

    working hardcode

--- schema.js
+++ schema.js
@@ -1,46 +1,59 @@
-const axios = require('axios');
-
-const {  
-    GraphQLObjectType, 
-    GraphQLString, 
-    GraphQLList,
+//const axios = require('axios');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
     GraphQLSchema,
+    GraphQLList,
     GraphQLNonNull
-}= require('graphql');
+} = require('graphql');
 
-// Gif information
-const GifType = new GraphQLObjectType({
-    name: 'Gif',
-    fields: () => ({
-        id: {type : new GraphQLNonNull(GraphQLString)},
-        bitly_gif_url: { type: GraphQLString },
-        rating: { type: GraphQLString },
-        images : { type: GifImage }
-    })
-});
 
-// Image specification
-const GifImage = new GraphQLObjectType({
-    name: 'Image',
-    fields: () => ({
-        original: { type: GraphQLString }
+// Hardcoded data
+const tasks = [
+    {taskNum: 1, task: "Code More", progress: "Doing", difficulty: 6},
+    {taskNum: 2, task: "Read More", progress: "Do", difficulty: 4},
+    {taskNum: 3, task: "Workout More", progress: "Done", difficulty: 9}
+];
+
+
+// Task Type
+const ToDoType = new GraphQLObjectType({
+    name:'Task',
+    fields:() => ({
+        taskNum: {type:GraphQLInt},
+        task: {type: GraphQLString},
+        progress: {type: GraphQLString},
+        difficulty: {type: GraphQLInt},
     })
 });
 
 // Root Query
-const RootQuery = new GraphQLObjectType({
-    name : 'RootQueryType',
-    fields: {
-        id: {
-            type: new GraphQLList(GifType),
-            resolve(parent, args) {
-                return axios
-                .get(`https://api.giphy.com/v1/gifs/trending&api_key=tY3kykAKF4BORze5adGtAziQjfy0PYhz`)
-                .then(res => res.data);
+const RootQuery= new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:{
+        task:{
+            type: ToDoType,
+            args:{
+                taskNum:{type:GraphQLInt}
+            },
+            resolve(parentValue, args){
+                for(let i = 0;i < tasks.length;i++){
+                    if(tasks[i].taskNum === args.taskNum){
+                        return tasks[i];
+                    }
+                }
+            }
+        },
+        allTasks:{
+            type: new GraphQLList(ToDoType),
+            resolve(parentValue, args){
+                return tasks;
             }
         }
     }
 });
+
 
 module.exports = new GraphQLSchema({
     query: RootQuery
