a1f95242b9d6af475835933cd2b1eaf618714aaf
imalam64 <imalam64@gmail.com>
2018-11-28 21:12:21 -0800

    working side drawer

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -21,22 +23,55 @@ const styles = {
   },
 };
 
-function NavBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
+class NavBar extends React.Component {
+  state = {
+    left: false
+  }
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    const sideList = (
+      <div className={classes.list}>
+        <List>
+          {/* Add links to GitHub and Profiles here */}
+          <ListItem>Love!</ListItem>
+        </List>
+      </div>
+    );
+
+    return (
+   <div>
       <AppBar position="static">
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
+          <IconButton onClick={this.toggleDrawer('left', true)} className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
           </IconButton>
-          <Typography  color="inherit" className={classes.grow}>
-            <h1>Welcome to the best To-Do List!</h1>
+          <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)}>
+            <div
+              tabIndex={0}
+              role="button"
+              onClick={this.toggleDrawer('left', false)}
+              onKeyDown={this.toggleDrawer('left', false)}
+            >
+              {sideList}
+          </div>
+          </Drawer>
+          <Typography variant="h5" color="inherit" className={classes.grow}>
+            Welcome to the best To-Do List!
           </Typography>
         </Toolbar>
       </AppBar>
     </div>
-  );
+    )
+  }
+
 }
 
 NavBar.propTypes = {
